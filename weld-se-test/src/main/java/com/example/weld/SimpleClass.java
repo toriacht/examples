a9d5068454759fa450d10a0e02d9a2dc99e31873
marko <milenkovic.m@gmail.com>
2012-01-29 14:17:05 +0000

    initial import

---
+++ weld-se-test/src/main/java/com/example/weld/SimpleClass.java
@@ -0,0 +1,8 @@
+package com.example.weld;
+
+public class SimpleClass {
+	public String sayHello(String name){
+		return "Hello "+name.toUpperCase();
+	}
+
+}
---
+++ weld-se-test/src/test/java/com/example/weld/SimpleClassTest.java
@@ -0,0 +1,37 @@
+package com.example.weld;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class SimpleClassTest {
+	
+	@Inject
+	SimpleClass simple;
+	
+	@Deployment	
+	public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(JavaArchive.class, "archive.jar").
+    		  addAsResource("META-INF/beans.xml", "META-INF/beans.xml").
+    		  addPackage("com.example.weld");
+	}
+	
+	@Test	
+	public void first_test(){
+		Assert.assertNotNull(this.simple);
+	}
+	
+	@Test	
+	public void first_second(){
+		String name = "marko";
+		Assert.assertEquals("Hello "+name.toUpperCase(), this.simple.sayHello(name));
+	}
+}
